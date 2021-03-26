c657f4c040c54f3588c9a33959ac44cd10e93cfc
Daniel <danielzamignani@gmail.com>
2021-03-26 08:01:13 -0300

    Challenge Finished

--- src/modules/users/model/User.ts
+++ src/modules/users/model/User.ts
@@ -1,7 +1,19 @@
 import { v4 as uuidV4 } from "uuid";
 
 class User {
-  // Complete aqui
+    name: string;
+    email: string;
+    updated_at: Date;
+    created_at: Date;
+    id?: string;
+    admin?: boolean;
+
+    constructor() {
+        if (!this.id) {
+            this.id = uuidV4();
+            this.admin = false;
+        }
+    }
 }
 
 export { User };
